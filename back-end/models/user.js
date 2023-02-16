2a1c097bd1db5cdc9b859a181ef9c2689f090730
Anton <krava34007@gmail.com>
2023-02-16 19:37:12 +0000

    add UserSchema

---
+++ back-end/models/user.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  isEmailVerified: { type: Boolean, default: false },
+  activationLink: { type: String },
+});
+
+module.exports = model("User", UserSchema);
