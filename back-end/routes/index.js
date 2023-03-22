8f868bee7ad8db37197a7ec22c442651c34314a8
Anton <krava34007@gmail.com>
2023-03-22 14:26:49 +0000

    update router

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,5 +1,19 @@
 const Router = require("express").Router;
 
+const register = require("../controllers/register");
+const login = require("../controllers/login");
+const logout = require("../controllers/logout");
+const activateEmail = require("../controllers/activateEmail");
+const refresh = require("../controllers/refresh");
+const getUsers = require("../controllers/getUsers");
+
 const router = new Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/activate-email/:link", activateEmail);
+router.get("/refresh", refresh);
+router.get("/users", getUsers);
 
 module.exports = router;
