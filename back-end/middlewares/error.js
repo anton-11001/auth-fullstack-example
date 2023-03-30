c1fa44812c070ff66ff4a661a4edd8bd7b7cd5a2
Anton <krava34007@gmail.com>
2023-03-30 17:59:12 +0000

    add errorMiddleware

---
+++ back-end/middlewares/error.js
@@ -0,0 +1,15 @@
+const errorMiddleware = (err, req, res, next) => {
+  console.error(err);
+
+  if (err.status) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: "Unexpected server error",
+  });
+};
+
+module.exports = errorMiddleware;
