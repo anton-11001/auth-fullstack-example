13901932928e304c1c91aa6ee901ef29617e7af9
Anton <krava34007@gmail.com>
2023-06-03 13:27:18 +0000

    add logout

--- back-end/controllers/logout.js
+++ back-end/controllers/logout.js
@@ -1,3 +1,14 @@
-const logout = (req, res, next) => {};
+const deleteToken = require("../db/tokens/deleteToken");
+
+const logout = async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+    await deleteToken(refreshToken);
+    res.clearCookie("refreshToken");
+    return res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = logout;
