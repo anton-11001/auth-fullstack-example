0722bdd96af5da7251f8edbba7480a6c0f93ef1b
Anton <krava34007@gmail.com>
2023-06-30 16:21:09 +0000

    add getUsers

--- back-end/controllers/getUsers.js
+++ back-end/controllers/getUsers.js
@@ -1,3 +1,12 @@
-const getUsers = (req, res, next) => {};
+const getUsersFromDb = require("../db/user/getUsers");
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await getUsersFromDb();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = getUsers;
