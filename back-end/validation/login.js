defdd5366110a59ce4e0f4702d3b069141fc7c40
Anton <krava34007@gmail.com>
2023-08-01 08:23:17 +0000

    update loginValidation

--- back-end/validation/login.js
+++ back-end/validation/login.js
@@ -4,6 +4,9 @@ const loginValidation = [
   body("email")
     .isEmail()
     .withMessage("Please provide a valid email address"),
+  body("password")
+    .isLength({ min: 3, max: 32 })
+    .withMessage("Password must be between 3 and 32 characters"),
 ];
 
 module.exports = loginValidation;
