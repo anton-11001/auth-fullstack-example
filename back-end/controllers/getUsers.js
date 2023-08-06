85edb6eed80decb10d25bafb8f708fd87b719e3a
Anton <krava34007@gmail.com>
2023-08-06 10:18:39 +0000

    update getUsers

--- back-end/controllers/getUsers.js
+++ back-end/controllers/getUsers.js
@@ -1,9 +1,10 @@
 const getUsersFromDb = require("../db/user/getUsers");
+const UserDto = require("../dtos/user");
 
 const getUsers = async (req, res, next) => {
   try {
     const users = await getUsersFromDb();
-    res.json(users);
+    res.json(users.map((user) => new UserDto(user)));
   } catch (error) {
     next(error);
   }
