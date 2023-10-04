99a1c14eb3613f62fd4bef1393cf4f3ed6854b51
Anton <krava34007@gmail.com>
2023-10-04 13:29:58 +0000

    add app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,21 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import React, { FC } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { queryClient } from "./lib/tanstack-query/query-client";
+import { AppRoutes } from "./router";
+import "./styles.css";
 
 const App: FC = () => {
-  return <div>Hello world</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
