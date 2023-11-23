9c6bf162fad5c19ffca9583f640685a38b33d5b6
Anton <krava34007@gmail.com>
2023-11-23 13:21:56 +0000

    update input

--- front-end/src/components/ui/Input.tsx
+++ front-end/src/components/ui/Input.tsx
@@ -1,18 +1,20 @@
-import React, { FC, InputHTMLAttributes } from "react";
+import React, { forwardRef, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: string;
   label: string;
 }
 
-export const Input: FC<InputProps> = ({ error, label, id, ...props }) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ error, label, id, ...props }, ref) => {
   const inputId = id || props.name;
 
   return (
     <label className="field" htmlFor={inputId}>
       <span className="field__label">{label}</span>
-      <input className={`input ${error ? "input--error" : ""}`} id={inputId} {...props} />
+      <input ref={ref} className={`input ${error ? "input--error" : ""}`} id={inputId} {...props} />
       {error ? <span className="field__error">{error}</span> : null}
     </label>
   );
-};
+});
+
+Input.displayName = "Input";
