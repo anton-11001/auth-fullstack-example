6b0ab6b64894afff42e72d940d8a46b9ee89c4f7
Anton <krava34007@gmail.com>
2023-12-01 08:19:36 +0000

    update generateTokens

--- back-end/services/token/generateTokens.js
+++ back-end/services/token/generateTokens.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const generateTokens = (payload) => {
-  const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+  const tokenPayload = { ...payload };
+
+  const accessToken = jwt.sign(tokenPayload, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "15m",
   });
 
-  const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+  const refreshToken = jwt.sign(tokenPayload, process.env.JWT_REFRESH_SECRET, {
     expiresIn: "30d",
   });
 
