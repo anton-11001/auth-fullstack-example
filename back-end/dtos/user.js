1bbd25cfda793f276d401dce7ba2645893abac8e
Anton <krava34007@gmail.com>
2023-12-04 13:42:11 +0000

    update UserDto

--- back-end/dtos/user.js
+++ back-end/dtos/user.js
@@ -1,6 +1,6 @@
 class UserDto {
   constructor(user) {
-    this.id = user._id;
+    this.id = user._id.toString();
     this.email = user.email;
     this.name = user.name;
     this.isEmailVerified = user.isEmailVerified;
